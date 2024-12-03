a160c0afa1ea947bae1f5b3e17c80bab57e171e6
joseckratemo343@gmail.com <joseckratemo343@gmail.com>
2024-12-04 01:04:14 +0300

    updates

--- Biasharaplusapp/urls.py
+++ Biasharaplusapp/urls.py
@@ -9,6 +9,11 @@ from Biasharaplusapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',views.home),
-    path('login/',views.login_views,name='login'),
-    path('register/',views.register_views,name='register'),
+    path('login/',views.login,name='login'),
+    path('register/',views.register,name='register'),
+    path('index/',views.index,name='index'),
+    path('starter-page/',views.starter,name='starter-page'),
+    path('service-details/',views.service,name='service-details'),
+    path('blog/',views.blog,name='blog'),
+    path('blog-details/',views.details,name='blog-details'),
 ]
--- Biasharaplusapp/views.py
+++ Biasharaplusapp/views.py
@@ -4,8 +4,23 @@ from django.shortcuts import render
 def home(request):
     return render(request,'index.html')
 
-def login_views(request):
+def login(request):
     return render(request,'login.html')
 
-def register_views(request):
+def register(request):
     return render(request, 'register.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def service(request):
+    return render(request, 'service-details.html')
+
+def starter(request):
+    return render(request, 'starter-page.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def details(request):
+    return render(request, 'blog-details.html')
